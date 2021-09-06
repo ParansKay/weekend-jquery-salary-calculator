c8eb7874a1157c1a78295dc29dd0b9cc1e4953a9
Paran Kashani <parankashani@gmail.com>
2021-09-06 16:34:16 -0500

    table set, delete button works both on html and js, total cost works

--- Scripts/client.js
+++ Scripts/client.js
@@ -4,12 +4,12 @@ const budget = 20000;
 let employees = [];
 
 
-function addEmployee (){
+function addEmployee(){
     console.log( 'in addEmployee:');
     let newEmployees = {
         fName: $( '#firstNameIn' ).val(),
         lName: $( '#lastNameIn' ).val(),
-        idNum: $( '#idNumberIn' ).val(),
+        idNum: Number($( '#idNumberIn' ).val()),
         job: $( '#jobTitleIn' ).val(),
         salary: $( '#annualSalaryIn' ).val(),
     };
@@ -22,38 +22,79 @@ function addEmployee (){
     $( '#jobTitleIn' ).val( '' );
     $( '#annualSalaryIn' ).val( '' );
     // calculate remaining budget
-    calculateRemainingBudget();
+    calculateRemainingTotalCost();
     displayEmployees();
+    console.log(newEmployees);
 }; // end addEmployee
-function calculateRemainingBudget(){
-    console.log( 'in calculateRemainingBudget:' );
-    //loop through purchases
-    let totalPrice = 0;
+
+
+function byeEmployee(){
+    console.log( 'in byeEmployee' );
+    $( this ).parent().parent().remove();
+    //find the employee we want to delete
+    let employeeToDelete = Number($( this ).parent().parent().find( '.theIDNum' ).text());
+    console.log( 'employee to delete:', employeeToDelete );
+    // create an array where removed employees are stored
+    let holdTheseEmployees = [];
     for( let i = 0; i<employees.length; i++ ){
-        totalPrice += Number( employees[i].salary );
+        if( employeeToDelete === employees[i].idNum ){
+        console.log( 'deleting this employee:', employeeToDelete );
+        } // end if 
+        else{
+        holdTheseEmployees.push( employees[i] );    
+        }
+    } // end for loop
+    employees = holdTheseEmployees;
+    // remove them from the employees array
+    // console log the array
+};// end byeEmployee
+
+function calculateRemainingTotalCost(){
+    console.log( 'in calculateTotalCost:' );
+    //loop through purchases
+    let totalCost = 0;
+    for( let i = 0; i<employees.length; i++ ){
+        totalCost += Number( employees[i].salary );
     } // end for 
-    console.log( 'totalPrice:', totalPrice );
-        const remainingBudget = budget - totalPrice;
-        let el = $( '#remainingBudgetOut' );
+    console.log( 'totalCost:', totalCost );
+        const remainingBudget = budget - totalCost;
+        let el = $( '#remainingTotalCostOut' );
         el.empty();
         el.append( remainingBudget );
+        if( totalCost > budget ){
+            $( '#remainingTotalCostOut' ).css( 'background-color' , 'red' );
+            $( '#remainingTotalCostOut' ).css( 'color' , 'white' );
+
+         } // end if
     // for each employee, add up all the salaries
     // subtract totalPrice from budget for remainingBudget
     // display remaining budget
     // 
 }; //end calculateRemainingBudget
+
 function displayEmployees(){
 console.log( 'in displayEmployees: ' );
 let el = $( '#employeesOut' );
 el.empty();
-    for( employee of employees ){
-        el.append( '<li>' + employees.fName + ' : $EMPLOYEESALARY </li>' ); 
+    for( newEmployees of employees ){
+        el.append(
+            `<tr>
+            <td>${newEmployees.fName}</td>
+            <td>${newEmployees.lName}</td>
+            <td class="theIDNum">${newEmployees.idNum}</td>
+            <td>${newEmployees.job}</td>
+            <td>${newEmployees.salary}</td>
+            <td><button id="deleteEmployee">DELETE</button></td>
+            </tr>`)
     }// end for 
 } // end displayEmployees
+
 function readyNow(){
     let el = $( '#budgetOut');
     el.empty();
     el.append( budget );
     $( '#submitButton' ).on( 'click', addEmployee );
-    calculateRemainingBudget();
+    $( '#employeesOut' ).on( 'click', `#deleteEmployee`, byeEmployee); 
+    calculateRemainingTotalCost();
+
 }; // readyNow
